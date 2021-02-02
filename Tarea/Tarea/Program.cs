cf0d98973071d65620a0ee80fb917ab06b7f072e
astrid-Gomez325 <78381465+astrid-Gomez325@users.noreply.github.com>
2021-02-02 11:33:28 -0400

    Add files via upload

---
+++ Tarea/Tarea/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int c = 1;
+            int aacb = 2214;
+
+            Console.WriteLine("AACB");
+            
+            Console.WriteLine(aacb) ;
+
+            Console.WriteLine("BCA");
+            int bca = 412;
+            Console.WriteLine(bca);
+            Console.WriteLine("BCBC");
+            int bcbc = 4141;
+            Console.WriteLine(bcbc);
+            
+            
+            Console.WriteLine("Pura mente 7u7 JAJAJAJAJA");
+            Console.WriteLine("esto se parece a una mezcla entre HTML y Pseint jijijiji");
+            Console.WriteLine("pulse cualquier tecla para terminar y cerrar ventana");
+
+            Console.ReadKey(true);
+
+
+
+        }
+    }
+}
